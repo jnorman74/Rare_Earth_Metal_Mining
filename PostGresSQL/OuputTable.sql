2c5e6b84447bc3c2e3a6f74ed2d052a84a16bfc0
sholkojr <stan.holko@hotmail.com>
2021-02-02 21:56:14 -0500

    Cleanup

--- PostGresSQL/OuputTable.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS output CASCADE;
-
-CREATE TABLE output (
-	sample_id FLOAT,
-	rare_earth_predict FLOAT
-	latitude FLOAT,
-	longitude FLOAT,
-);
--- SQL/Sample_Major_Trace_TableJoin.sql
+++
@@ -1,6 +0,0 @@
-SELECT * 
-FROM "Sample" s
-INNER JOIN "Major" m ON (s.major_id = m.major_id)
-INNER JOIN "Trace" t ON (s.trace_id = t.trace_id)
-INNER JOIN "Computed" c ON (s.comp_id = c.comp_id)
-ORDER BY s.sample_id;
