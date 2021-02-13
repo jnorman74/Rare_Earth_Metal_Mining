150f3cdc3139926197549487ed227e236cfd9af9
Jamie Norman <69658824+jnorman74@users.noreply.github.com>
2021-02-13 15:29:50 -0500

    Updated app.py and logic.js files...

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -10,7 +10,7 @@ import psycopg2
 from config import db_password
 
 class DbUtils:
-    def getSample(self):
+    """ def getSample(self):
         # Create database string
         db_string = f'postgres://vvqxjory:{db_password}@ziggy.db.elephantsql.com:5432/vvqxjory'
         # Create an engine instance
@@ -19,7 +19,7 @@ class DbUtils:
         dbConnection = engine.connect()
         # Read data from PostgreSQL database table and load into a DataFrame instance
         sample = engine.execute("SELECT * from \"canada_samples\" ", dbConnection)
-        return sample
+        return sample """
 
     def getOutput(self):
         # Create database string
@@ -29,8 +29,30 @@ class DbUtils:
         # Connect to PostgreSQL server
         dbConnection = engine.connect()
         # Read data from PostgreSQL database table and load into a DataFrame instance
-        output = engine.execute("SELECT * from \"output_positive\" ", dbConnection)
+        output = engine.execute("SELECT * from \"output_positive\" where rare_earth > 0", dbConnection)
         return output
+
+    def getGold(self):
+        # Create database string
+        db_string = f'postgres://vvqxjory:{db_password}@ziggy.db.elephantsql.com:5432/vvqxjory'
+        # Create an engine instance
+        engine = create_engine(db_string)
+        # Connect to PostgreSQL server
+        dbConnection = engine.connect()
+        # Read data from PostgreSQL database table and load into a DataFrame instance
+        gold = engine.execute("SELECT * from \"gold_country\" ", dbConnection)
+        return gold
+
+    def getSilver(self):
+        # Create database string
+        db_string = f'postgres://vvqxjory:{db_password}@ziggy.db.elephantsql.com:5432/vvqxjory'
+        # Create an engine instance
+        engine = create_engine(db_string)
+        # Connect to PostgreSQL server
+        dbConnection = engine.connect()
+        # Read data from PostgreSQL database table and load into a DataFrame instance
+        silver = engine.execute("SELECT * from \"silver_country\" ", dbConnection)
+        return silver
 
 app = Flask(__name__)
 CORS(app)
@@ -39,7 +61,7 @@ CORS(app)
 def welcome():
     return render_template("index.html")
 
-@app.route('/api/v1.0/cdn_samples',methods=['GET'])  
+""" @app.route('/api/v1.0/cdn_samples',methods=['GET'])  
 def getSample():  
     samples = []  
     dbUtils = DbUtils()  
@@ -48,7 +70,7 @@ def getSample():
         a = {"sample_id": r[0], "sample_name": r[1], "latitude": r[2], "longtitude": r[3], "rock_name": r[4], "rock_type": r[5], "country": r[6]}
         samples.append(a)
       
-    return jsonify(samples)
+    return jsonify(samples) """
 
 @app.route('/api/v1.0/output_positive',methods=['GET'])  
 def getOutput():  
@@ -59,7 +81,29 @@ def getOutput():
         a = {"sample_id": r[1], "rare_earth_predict": r[2], "rare_earth": r[3], "latitude": r[4], "longtitude": r[5], "au_ppm": r[6], "ag_ppm": r[7], "sc_ppm": r[9], "y_ppm": r[10], "la_ppm": r[11], "ce_ppm": r[12], "pr_ppm": r[13], "nd_ppm": r[14], "sm_ppm": r[15], "eu_ppm": r[16], "gd_ppm": r[17], "tb_ppm": r[18], "dy_ppm": r[19], "ho_ppm": r[20], "er_ppm": r[21], "tm_ppm": r[22], "yb_ppm": r[23], "lu_ppm": r[24], "country": r[8]}
         outputs.append(a)
       
-    return jsonify(outputs)  
+    return jsonify(outputs) 
+
+@app.route('/api/v1.0/gold',methods=['GET'])  
+def getGold():  
+    gold = []  
+    dbUtils = DbUtils()  
+    goldData = dbUtils.getGold()
+    for r in goldData:
+        a = {"sample_id": r[0], "sample_name": r[1], "latitude": r[2], "longtitude": r[3], "rock_name": r[4], "au_ppm": r[5], "country": r[6]}
+        gold.append(a) 
+
+    return jsonify(gold)
+
+@app.route('/api/v1.0/silver',methods=['GET'])  
+def getSilver():  
+    silver = []  
+    dbUtils = DbUtils()  
+    silverData = dbUtils.getSilver()
+    for r in silverData:
+        a = {"sample_id": r[0], "sample_name": r[1], "latitude": r[2], "longtitude": r[3], "rock_name": r[4], "ag_ppm": r[5], "country": r[6]}
+        silver.append(a) 
+
+    return jsonify(silver) 
   
 class JSONObject:  
   def __init__( self, dict ):  
--- Dashboard/static/js/logic.js
+++ Dashboard/static/js/logic.js
@@ -1,21 +1,21 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the tile layer that will be the background of our map.
+// Create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
-// We create the second tile layer that will be the background of our map.
+// Create second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 
-// We create the third tile layer that will be the background of our map.
+// Create the third tile layer that will be the background of our map.
 let darkBack = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
@@ -29,7 +29,7 @@ let map = L.map('mapid', {
 	layers: [darkBack]
 });
 
-// Create a base layer that holds all three maps.
+// Create a base layer that holds all maps.
 let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets,
@@ -37,19 +37,23 @@ let baseMaps = {
 };
 
 // Add a layer group.
-let cdnSamples = new L.LayerGroup();
+//let cdnSamples = new L.LayerGroup();
 let mlOutputs = new L.LayerGroup();
+let goldPPM = new L.LayerGroup();
+let silverPPM = new L.LayerGroup();
 
-// 2. Add a reference group to the overlays object.
+// Add a reference group to the overlays object.
 let overlays = {
-  "Samples": cdnSamples,
-  "ML Outputs": mlOutputs
+  // "Samples": cdnSamples,
+  "ML Outputs": mlOutputs,
+  "Gold PPM": goldPPM,
+  "Silver PPM": silverPPM
 };
 
 // Then we add a control to the map that will allow the user to change which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
+L.control.layers(baseMaps, overlays, collapsed=false).addTo(map);
 
-  // Retrieve the API JSON data from ElephantSQL DB.
+/*   // Retrieve the API JSON data from ElephantSQL DB.
   d3.json("http://127.0.0.1:5000/api/v1.0/cdn_samples").then(function(data) {
 
     // Restructure the JSON data to geoJSON
@@ -104,7 +108,7 @@ L.control.layers(baseMaps, overlays).addTo(map);
   // Then we add the json layer to our map.
   cdnSamples.addTo(map);
 
-  });
+  }); */
 
   // Retrieve the API JSON data from ElephantSQL DB.
   d3.json("http://127.0.0.1:5000/api/v1.0/output_positive").then(function(data) {
@@ -128,7 +132,7 @@ L.control.layers(baseMaps, overlays).addTo(map);
         
     });
 
-     //Style options for the Sample Points
+     // Style options for the Sample Points
      function styleInfo(feature) {
        return {
       radius: getRadius(feature.properties.rare_earth),
@@ -143,44 +147,44 @@ L.control.layers(baseMaps, overlays).addTo(map);
 
   // This function determines the colour of the marker based on the total number of rare earth minerals
   function getColor(rare_earth) {
-    if (rare_earth > 100000) {
-      return "#ea2c2c";
+    if (rare_earth >= 100000) {
+      return "#a50f15";
     }
-    if (rare_earth > 50000) {
-      return "#ea822c";
+    if (rare_earth >= 50000) {
+      return "#de2d26";
     }
-    if (rare_earth > 10000) {
-      return "#ee9c00";
+    if (rare_earth >= 10000) {
+      return "#fb6a4a";
     }
-    if (rare_earth > 1000) {
-      return "#eecc00";
+    if (rare_earth >= 5000) {
+      return "#fc9272";
     }
-    if (rare_earth < 1000) {
-      return "#d4ee00";
+    if (rare_earth >= 1000) {
+      return "#fcbba1";
     }
-    if (rare_earth == 0) {
-    return "#98ee00"
+    if (rare_earth > 0) {
+    return "#fee5d9"
     }
   }
 
   // This function determines the radius of the earthquake marker based on the rare earth totals
   function getRadius(rare_earth) {
-    if (rare_earth > 100000) {
+    if (rare_earth >= 100000) {
+      return 30;
+    }
+    if (rare_earth >= 50000) {
       return 20;
     }
-    if (rare_earth > 50000) {
+    if (rare_earth >= 10000) {
       return 15;
     }
-    if (rare_earth > 10000) {
+    if (rare_earth >= 5000) {
       return 10;
     }
-    if (rare_earth > 999) {
+    if (rare_earth >= 1000) {
       return 5;
     }
-    if (rare_earth < 1000) {
-      return 2.5;
-    }
-    if (rare_earth === 0) {
+    if (rare_earth > 0) {
     return 1;
     }
   }
@@ -206,5 +210,213 @@ L.control.layers(baseMaps, overlays).addTo(map);
 
   // Then we add the json layer to our map.
   mlOutputs.addTo(map);
+
+  });
+
+  // Retrieve the API JSON data from ElephantSQL DB.
+  d3.json("http://127.0.0.1:5000/api/v1.0/gold").then(function(data) {
+
+  // Restructure the JSON data to geoJSON
+    var jsonFeatures = [];
+
+    data.forEach(function(point){
+      var lat = point.latitude;
+      var lon = point.longtitude;
+
+      var feature = {type: 'Feature',
+          properties: point,
+          geometry: {
+              type: 'Point',
+              coordinates: [lon,lat]
+          }
+        };
+
+        jsonFeatures.push(feature);
+        
+    });
+
+     // Style options for the Sample Points
+     function styleInfo(feature) {
+      return {
+     radius: getRadius(feature.properties.au_ppm),
+     fillColor: getColor(feature.properties.au_ppm),
+     color: "#000000",
+     weight: 0.5,
+     opacity: 1,
+     fillOpacity: 0.8,
+     stroke: true
+   };
+ }
+
+ // This function determines the colour of the marker based on the total number of rare earth minerals
+ function getColor(au_ppm) {
+   if (au_ppm >= 100000) {
+     return "#993404";
+   }
+   if (au_ppm >= 10000) {
+     return "#d95f0e";
+   }
+   if (au_ppm >= 1000) {
+     return "#fe9929";
+   }
+   if (au_ppm >= 100) {
+     return "#fec44f";
+   }
+   if (au_ppm >= 1) {
+     return "#fee391";
+   }
+   if (au_ppm < 1) {
+   return "#ffffd4"
+   }
+ }
+
+ // This function determines the radius of the earthquake marker based on the rare earth totals
+ function getRadius(au_ppm) {
+   if (au_ppm >= 100000) {
+     return 30;
+   }
+   if (au_ppm >= 10000) {
+     return 20;
+   }
+   if (au_ppm >= 1000) {
+     return 15;
+   }
+   if (au_ppm >= 100) {
+     return 10;
+   }
+   if (au_ppm >= 10) {
+     return 2.5;
+   }
+   if (au_ppm < 10) {
+   return 1;
+   }
+ }
+    var geoJSON = { type: 'FeatureCollection', features: jsonFeatures};
+
+    L.geoJson(geoJSON, {
+
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+    //Set the style for eash circleMarker on the map
+    style: styleInfo,
+
+    // Create a popup for each circleMarker to display attribute info
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<b>Sample ID:</b> " + feature.properties.sample_id + "<br><b>Sample Name: </b>" + feature.properties.sample_name + "<br><b>Rock Name: </b>" + feature.properties.rock_name + "<br><b>Gold PPM: </b>" + feature.properties.au_ppm + "<br><b>Country: </b>" + feature.properties.country);
+      }
+
+    }).addTo(goldPPM);
+  
+
+  // Then we add the json layer to our map.
+  goldPPM.addTo(map);
+
+  });
+
+// Retrieve the API JSON data from ElephantSQL DB.
+d3.json("http://127.0.0.1:5000/api/v1.0/silver").then(function(data) {
+
+  // Restructure the JSON data to geoJSON
+    var jsonFeatures = [];
+
+    data.forEach(function(point){
+      var lat = point.latitude;
+      var lon = point.longtitude;
+
+      var feature = {type: 'Feature',
+          properties: point,
+          geometry: {
+              type: 'Point',
+              coordinates: [lon,lat]
+          }
+        };
+
+        jsonFeatures.push(feature);
+        
+    });
+
+     // Style options for the Sample Points
+     function styleInfo(feature) {
+      return {
+     radius: getRadius(feature.properties.ag_ppm),
+     fillColor: getColor(feature.properties.ag_ppm),
+     color: "#000000",
+     weight: 0.5,
+     opacity: 1,
+     fillOpacity: 0.8,
+     stroke: true
+   };
+ }
+
+ // This function determines the colour of the marker based on the number of gold ppm
+ function getColor(ag_ppm) {
+   if (ag_ppm >= 1000) {
+     return "#4a1486";
+   }
+   if (ag_ppm >= 500) {
+     return "#6a51a3";
+   }
+   if (ag_ppm >= 100) {
+     return "#807dba";
+   }
+   if (ag_ppm >= 50) {
+     return "#9e9ac8";
+   }
+   if (ag_ppm >= 10) {
+     return "#bcbddc";
+   }
+   if (ag_ppm >= 1) {
+   return "#dadaeb"
+   }
+   if (ag_ppm <1) {
+     return "#f2f0f7"
+   }
+ }
+
+ // This function determines the radius of the earthquake marker based on the rare earth totals
+ function getRadius(ag_ppm) {
+   if (ag_ppm >= 10000) {
+     return 25;
+   }
+   if (ag_ppm >= 500) {
+     return 20;
+   }
+   if (ag_ppm >= 100) {
+     return 15;
+   }
+   if (ag_ppm >= 50) {
+     return 10;
+   }
+   if (ag_ppm >= 10) {
+     return 5;
+   }
+   if (ag_ppm >= 1) {
+   return 2.5;
+   }
+   if (ag_ppm < 1) {
+     return 1;
+   }
+ }
+    var geoJSON = { type: 'FeatureCollection', features: jsonFeatures};
+
+    L.geoJson(geoJSON, {
+
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+    //Set the style for eash circleMarker on the map
+    style: styleInfo,
+
+    // Create a popup for each circleMarker to display attribute info
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<b>Sample ID:</b> " + feature.properties.sample_id + "<br><b>Sample Name: </b>" + feature.properties.sample_name + "<br><b>Rock Name: </b>" + feature.properties.rock_name + "<br><b>Silver PPM: </b>" + feature.properties.ag_ppm + "<br><b>Country: </b>" + feature.properties.country);
+      }
+
+    }).addTo(silverPPM);
+  
+
+  // Then we add the json layer to our map.
+  silverPPM.addTo(map);
 
   });
